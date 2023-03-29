c5e1adc4f83492add9702027136d9e6e1f880699
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-03-30 00:10:44 +0530

    Create Module done with validation.

--- EntertainmentApp/Controllers/CategoryController.cs
+++ EntertainmentApp/Controllers/CategoryController.cs
@@ -18,9 +18,28 @@ namespace EntertainmentApp.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //GET
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name", "The DisplayOrder and Name cannont be same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);  
         }
     }
 }
--- EntertainmentApp/Models/Category.cs
+++ EntertainmentApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EntertainmentApp.Models
 {
@@ -8,6 +9,8 @@ namespace EntertainmentApp.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
