04d91b3bbc4eacbfcd7f7542b60f6f91259a6f0d
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-03-29 18:51:36 +0530

    Add project files.

---
+++ EntertainmentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EntertainmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
