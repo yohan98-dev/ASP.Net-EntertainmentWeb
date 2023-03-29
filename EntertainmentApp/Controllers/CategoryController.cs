ea6b29679361994b30607354604082361b137dcc
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-03-29 20:45:40 +0530

    Fetch Data from DB Done.

---
+++ EntertainmentApp/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using EntertainmentApp.Data;
+using EntertainmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntertainmentApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
